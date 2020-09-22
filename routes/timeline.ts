67d3284177e96202ca3275d9a32e4a2504d60026
Bruno Andrade <brunocea.1@gmail.com>
2020-09-23 00:48:21 +0100

    update timeline assertions, added timeline scriteria query

--- routes/timeline.ts
+++ routes/timeline.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { assertions_by_month, assertions_by_date_cumulative, get_distinct_dates } from "../models/timeline";
+import { by_month_assertions, assertions_by_date_cumulative, get_distinct_dates, by_month_scriteria } from "../models/timeline";
 
 const router = express.Router();
 
@@ -16,11 +16,24 @@ router.get('/timeline', async function (req, res, next) {
   }
 });
 
-router.get('/byMonth', async function (req, res, next) {
+router.get('/byMonthAS', async function (req, res, next) {
   try {
     let serverName = req.query.name;
     let filters = req.query.filters ? req.query.filters : {};
-    assertions_by_month(serverName, filters)
+    by_month_assertions(serverName, filters)
+      .then((result: any) => res.send(result))
+      .catch((err: any) => res.send(err));
+  } catch (err) {
+      console.log(err);
+      res.send(err);
+  }
+});
+
+router.get('/byMonthSC', async function (req, res, next) {
+  try {
+    let serverName = req.query.name;
+    let filters = req.query.filters ? req.query.filters : {};
+    by_month_scriteria(serverName, filters)
       .then((result: any) => res.send(result))
       .catch((err: any) => res.send(err));
   } catch (err) {
